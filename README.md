c58a42b5b1ec689253666cf7475c090d63e7cf86
Aleks Haran <nporock@gmail.com>
2024-03-30 19:48:52 +0200

    add backend
    add 5 endpoint for task

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ReactApp
+
+## Run application using docker
+
+```
+$ docker-compose
+```
+
+## Installation
+
+```
+$ yarn
+```
+
+## Running application
+
+### # run frontend
+
+```
+$ yarn dev
+```
+
+### # run backend
+
+```
+$ yarn server
+```
---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/tasks",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve("index.html"),
+        404: resolve("404.html"),
+      },
+    },
+  },
+});
